fff09a768e680d61c62fb273971dbd6d31e27f33
tjtempmail <tjtempmail@gmail.com>
2026-05-17 03:36:26 -0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X659B.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X659B-user \
+    twrp_X659B-userdebug \
+    twrp_X659B-eng
